597c2391f81c2a13080da80e7bbe1ac4dee4b6a5
Qeenon <codemasha@gmail.com>
2020-05-26 13:44:54 +0400

    minor corrections
    code to show msk time for tours
    
    check that tours are not passed yet by time

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -43,7 +43,7 @@ ping: shows shard latency", false)
 leave: from music channel
 play <url>: play an radio stream or youtube music", false)
       .field("warcraft commands", "today: show tournaments today (same with tomorrow or yesterday)
-weekends: show tours on weekends", false)
+weekends: show tours at weekend", false)
       .footer(|f| f.text("As explained by Maho Hiyajo, Amadeus' capabilities of storing memory data make it possible to back-up the data of a person's memories. This could be useful, for example, to fight against memory loss."))
       .colour((246, 111, 0)))) {
     error!("Error sending help message: {:?}", why);
--- src/commands/warcraft.rs
+++ src/commands/warcraft.rs
@@ -21,13 +21,14 @@ use std::io::BufReader;
 use chrono::prelude::*;
 use chrono::{ Duration, Utc };
 
-pub fn tour(ctx: &mut Context, msg: &Message, on : DateTime<Utc>) -> CommandResult {
+pub fn tour_internal(ctx: &mut Context, msg: &Message, on : DateTime<Utc>, passed_check : bool) -> CommandResult {
   let res = reqwest::blocking::get("https://warcraft3.info/ical-events")?;
   let buf = BufReader::new(res);
 
   let reader = ical::IcalParser::new(buf);
 
   let str_date_now = on.format("%Y%m%d").to_string();
+  let str_time_now = on.format("%H%M").to_string();
 
   let mut eventos : Vec<String> = Vec::new();
 
@@ -42,12 +43,30 @@ pub fn tour(ctx: &mut Context, msg: &Message, on : DateTime<Utc>) -> CommandResu
               if let Some(val) = ep.value {
                 if val.len() >= 8 {
                   let str_date = &val[..8];
-                  if str_date_now == str_date {
+
+                  let not_passed = if passed_check {
+                    if let Ok(local_utc_time) = str_time_now.parse::<i32>() {
+                      let str_hour_mins = &val[9..13];
+                      if let Ok(event_hours_mins) = str_hour_mins.parse::<i32>() {
+                        local_utc_time < event_hours_mins
+                      } else {true }
+                    } else { true }
+                  } else { true };
+
+                  if str_date_now == str_date && not_passed {
                     is_today = true;
                     if val.len() >= 14 {
                       let str_hour = &val[9..11];
                       let str_min = &val[11..13];
-                      evstr = format!("• {}:{} CET", str_hour, str_min);
+                      let msk =
+                        if let Ok(str_int) = str_hour.parse::<i32>() {
+                          let mut msk_h = str_int + 3;
+                          if msk_h >= 24 {
+                            msk_h = msk_h - 24;
+                          }
+                          format!(" ({}:{} MSK)", msk_h.to_string(), str_min)
+                        } else { String::from("") };
+                      evstr = format!("• {}:{} CET {}", str_hour, str_min, msk);
                     }
                   }
                 }
@@ -83,6 +102,10 @@ pub fn tour(ctx: &mut Context, msg: &Message, on : DateTime<Utc>) -> CommandResu
   Ok(())
 }
 
+pub fn tour(ctx: &mut Context, msg: &Message, on : DateTime<Utc>) -> CommandResult {
+  tour_internal(ctx, msg, on, false)
+}
+
 #[command]
 pub fn yesterday(ctx: &mut Context, msg: &Message) -> CommandResult {
   let yesterday : DateTime<Utc> = Utc::now() - Duration::days(1); 
@@ -92,7 +115,7 @@ pub fn yesterday(ctx: &mut Context, msg: &Message) -> CommandResult {
 #[command]
 pub fn today(ctx: &mut Context, msg: &Message) -> CommandResult {
   let today : DateTime<Utc> = Utc::now(); 
-  tour(ctx, msg, today)
+  tour_internal(ctx, msg, today, true)
 }
 
 #[command]
@@ -106,16 +129,17 @@ pub fn weekends(ctx: &mut Context, msg: &Message) -> CommandResult {
   let mut today : DateTime<Utc> = Utc::now();
   if today.weekday() == Weekday::Sun {
     channel_message(&ctx, &msg, "Sunday:");
-    let _ = tour(ctx, msg, today);
+    tour_internal(ctx, msg, today, true)?;
   } else {
+    let is_saturday = today.weekday() == Weekday::Sat;
     while today.weekday() != Weekday::Sat {
       today = today + Duration::days(1); 
     }
     channel_message(&ctx, &msg, "Saturday:");
-    let _ = tour(ctx, msg, today);
+    tour_internal(ctx, msg, today, is_saturday)?;
     let tomorrow : DateTime<Utc> = Utc::now() + Duration::days(1); 
     channel_message(&ctx, &msg, "Sunday:");
-    let _ = tour(ctx, msg, tomorrow);
+    tour(ctx, msg, tomorrow)?;
   }
   Ok(())
 }

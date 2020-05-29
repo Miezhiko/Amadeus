358b9368b6835eba95dfbd46cac5099ec17672ee
Qeenon <codemasha@gmail.com>
2020-05-29 15:41:42 +0400

    minor improvements for stats
    Signed-off-by: Qeenon <codemasha@gmail.com>

--- src/commands/pad.rs
+++ src/commands/pad.rs
@@ -67,7 +67,10 @@ struct Search {
 
 #[command]
 pub fn stats(ctx: &mut Context, msg: &Message, args : Args) -> CommandResult {
-  let args_msg = args.message();
+  let mut args_msg = args.message();
+  if args_msg.is_empty() {
+    args_msg = msg.author.name.as_str();
+  }
   let userx = if args_msg.contains("#") { String::from(args_msg) }
     else {
       let search_uri = format!("https://statistic-service.w3champions.com/api/ladder/search?gateWay=20&searchFor={}&gameMode=1&season=1", args_msg);
@@ -99,7 +102,7 @@ pub fn stats(ctx: &mut Context, msg: &Message, args : Args) -> CommandResult {
         let stat_str = format!("wins: {}, loses: {}, winrate: {}%", stat.wins, stat.losses, winrate);
         out.push((String::from(race), stat_str, false));
       }
-      let max_games : Option<&Stats> = stats.iter().max_by_key(|p| p.games);
+      let max_games : Option<&Stats> = stats.iter().max_by_key(|s| s.games);
       let max_games_race = if max_games.is_some() { max_games.unwrap().race } else { 0 };
       let main_race_avatar = match max_games_race {
           1 => "http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-18/256/Warcraft-3-Reign-of-Chaos-3-icon.png",
@@ -108,14 +111,36 @@ pub fn stats(ctx: &mut Context, msg: &Message, args : Args) -> CommandResult {
           8 => "http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-18/256/Warcraft-3-Reign-of-Chaos-icon.png",
           _ => "http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-31/256/Warcraft-II-new-2-icon.png"
         };
-        let footer = format!("Requested by {}", msg.author.name);
+      let mut main_race_colors = match max_games_race {
+          1 => (0, 0, 222),
+          2 => (222, 0, 0),
+          4 => (0, 222, 0),
+          8 => (155, 0, 143),
+          _ => (50, 120, 150)
+        };
+      // not very good implementation of weird lilyal idea
+      if stats.len() > 1 {
+        let not_main : Vec<&Stats> = stats.iter().filter(|s| s.race != max_games_race).collect();
+        if not_main.len() > 0 {
+          let (r,g,b) = main_race_colors;
+          main_race_colors = match max_games_race {
+            1 => (r+0/2, g+0/2, b+222/2),
+            2 => (r+222/2, g+0/2, b+0/2),
+            4 => (r+0/2, g+222/2, b+0/2),
+            8 => (r+155/2, g+0/2, b+143/2),
+            _ => (r+50/2, g+120/2, b+150/2)
+          };
+        }
+      }
+      let footer = format!("Requested by {}", msg.author.name);
+      let description = format!("BattleTag: {}", userx.as_str());
       if let Err(why) = msg.channel_id.send_message(&ctx, |m| m
         .embed(|e| e
           .title(name)
-          .description(userx.as_str())
+          .description(description)
           .thumbnail(main_race_avatar)
           .fields(out)
-          .colour((225, 222, 103))
+          .colour(main_race_colors)
           .footer(|f| f.text(footer)))) {
         error!("Error sending help message: {:?}", why);
       }

d3c96c1e4d6dce08a9d79f794f3d003388a8357c
Qeenon <codemasha@gmail.com>
2020-06-05 17:07:56 +0400

    use embed for quote
    Signed-off-by: Qeenon <codemasha@gmail.com>

--- src/commands/chat.rs
+++ src/commands/chat.rs
@@ -19,8 +19,15 @@ pub fn quote(ctx: &mut Context, msg: &Message) -> CommandResult {
   if msg.mentions.len() > 0 {
     let target = &msg.mentions[0];
     if let Some(q) = chain::make_quote(ctx, msg, target.id, 9000) {
-      let out = format!("Quote from {}: {}", target.name, q);
-      channel_message(ctx, msg, out.as_str());
+      let footer = format!("Requested by {}", msg.author.name);
+      if let Err(why) = msg.channel_id.send_message(&ctx, |m| m
+      .embed(|e| e
+        .author(|a| a.icon_url(&target.face()).name(&target.name))
+        .description(q)
+        .footer(|f| f.text(footer))
+      )) {
+        error!("Failed to quote {}, {:?}", target.name, why);
+      }
     } else {
       let out = format!("No idea about {}", target.name);
       channel_message(ctx, msg, out.as_str());

b700bd06de7a49de9274ef58d65145683f23076a
Qeenon <codemasha@gmail.com>
2020-06-16 18:05:43 +0400

    improve word counting thing :D
    Signed-off-by: Qeenon <codemasha@gmail.com>

--- src/commands/quiz.rs
+++ src/commands/quiz.rs
@@ -17,24 +17,26 @@ use std::str;
 use std::collections::HashMap;
 
 use std::iter::FromIterator;
+use unicase::Ascii;
 
 #[command]
 pub fn words(ctx: &mut Context, msg: &Message) -> CommandResult {
   if msg.attachments.len() > 0 {
     if let Ok(file) = &msg.attachments[0].download() {
-      let mut words_counter: HashMap<&str, u32> = HashMap::new();
+      let mut words_counter: HashMap<Ascii<&str>, u32> = HashMap::new();
       let big_string = str::from_utf8(&file)?;
-      for word in big_string.split_whitespace() {
-        match words_counter.get_mut(word) {
-          Some(cx) => { *cx += 1; },
-          _ => { words_counter.insert(word, 1); }
+      for word in big_string.split(&[' ',',','.',';',':','\n','\r'][..]) {
+        if word.is_empty() { continue; }
+        let strx = Ascii::new(word);
+        match words_counter.get_mut(&strx) {
+          Some(cx) => *cx += 1,
+          None => { words_counter.insert(strx, 1); }
         };
       }
       let mut vec_from_iter = Vec::from_iter(words_counter);
       vec_from_iter.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
       let top_x : Vec<String> =
-        vec_from_iter.into_iter()
-                     .take(5)
+        vec_from_iter.into_iter().take(5)
                      .map(|(k, v)| format!("{} : {}", k, v))
                      .collect();
       let output = top_x.join("\n");

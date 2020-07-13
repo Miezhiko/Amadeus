a6af8cadc3e3d5a0c197107219e0c2e126556ac7
Qeenon <codemasha@gmail.com>
2020-07-13 14:45:56 +0400

    ok, markdown doesn't work for footer
    Signed-off-by: Qeenon <codemasha@gmail.com>

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -65,9 +65,9 @@ note: replace <thing> in help with text without < > quotes")
 • **weekends**: show tours at weekend
 • **stats** *<nick>*: shows ladder race stats (nick - battle tag or tag without numbers) (without nick will use discord name)
 • **ongoing**: show ongoing solo matches
-• **lineup** <title>: | map player map2 player2+player3 (title is optional)", false)
+• **lineup** *<title>: | map player map2 player2+player3* (title is optional)", false)
       .footer(|f| f.text(
-"*As explained by Maho Hiyajo, Amadeus' capabilities of storing memory data make it possible to back-up the data of a person's memories. This could be useful, for example, to fight against memory loss.*"))
+"As explained by Maho Hiyajo, Amadeus' capabilities of storing memory data make it possible to back-up the data of a person's memories. This could be useful, for example, to fight against memory loss."))
       .colour((246, 111, 0)))).await {
     error!("Error sending help message: {:?}", why);
   }

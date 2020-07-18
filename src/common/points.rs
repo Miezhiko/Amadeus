0aee63575cbb2dfdb7f39e33ae5c1033c2e475b5
Qeenon <codemasha@gmail.com>
2020-07-18 21:19:42 +0400

    add some more checks for points (but still not sure if that will help)

--- src/common/points.rs
+++ src/common/points.rs
@@ -19,7 +19,7 @@ struct Points {
 
 fn get_storage() -> Storage<FileNvm> {
   if !Path::new(LSUF).exists() {
-    let f = FileNvm::create(LSUF, 666_666_666).unwrap();
+    let f = FileNvm::create(LSUF, 666666666).unwrap();
     let storage: Storage<FileNvm> = Storage::create(f).unwrap();
     storage
   } else {
@@ -44,21 +44,35 @@ pub async fn add_points( guild_id: u64
       Ok(mbdata) => {
         if let Some(mut data) = mbdata {
           let byte_data: &mut [u8] = data.as_bytes_mut();
-          let mut points : Points = bincode::deserialize(byte_data).unwrap();
-          points.count += new_points;
-          let new_bytes = bincode::serialize(&points).unwrap();
-          (*byte_data).copy_from_slice(&new_bytes[..]);
-          let added: bool = storage.put(&lump_id, &data).unwrap();
-          if added {
-            error!("error updating points");
+          if let Ok(mut points) = bincode::deserialize::<Points>(byte_data) {
+            points.count += new_points;
+            if let Ok(new_bytes) = bincode::serialize(&points) {
+              (*byte_data).copy_from_slice(&new_bytes[..]);
+              match storage.put(&lump_id, &data) {
+                Ok(added) => {
+                  if added {
+                    error!("error updating points");
+                  }
+                }, Err(not_added) => {
+                  error!("failed to add points {:?}", not_added);
+                }
+              }
+            }
           }
         } else {
           let points = Points { count: 0, streak: 0 };
-          let encoded: Vec<u8> = bincode::serialize(&points).unwrap();
-          let lump_data: LumpData = LumpData::new(encoded).unwrap();
-          let added: bool = storage.put(&lump_id, &lump_data).unwrap();
-          if !added {
-            error!("error on points initialization");
+          if let Ok(encoded) = bincode::serialize(&points) {
+            if let Ok(lump_data) = LumpData::new(encoded) {
+              match storage.put(&lump_id, &lump_data) {
+                Ok(added) => {
+                  if !added {
+                    error!("error on points initialization");
+                  }
+                }, Err(not_added) => {
+                  error!("error on points initialization {:?}", not_added);
+                }
+              }
+            }
           }
         }
         if let Err(khm) = storage.journal_sync() {
@@ -144,8 +158,13 @@ pub async fn get_points(guild_id: u64, user_id: u64) -> Result<u64, cannyls::Err
     if let Ok(mbdata) = storage.get(&lump_id) {
       if let Some(mut data) = mbdata {
         let byte_data: &mut [u8] = data.as_bytes_mut();
-        let points : Points = bincode::deserialize(byte_data).unwrap();
-        return Ok(points.count);
+        match bincode::deserialize::<Points>(byte_data) {
+          Ok(points) => return Ok(points.count),
+          Err(get_error) => {
+            error!("Get error: {:?}", get_error);
+            return Ok(0);
+          }
+        };
       }
     }
     Ok(0)
@@ -171,35 +190,49 @@ pub async fn add_win_points( guild_id: u64
       Ok(mbdata) => {
         if let Some(mut data) = mbdata {
           let byte_data: &mut [u8] = data.as_bytes_mut();
-          let mut points : Points = bincode::deserialize(byte_data).unwrap();
-          points.count += 10;
-          points.streak += 1;
-          if points.streak > 3 {
-            let points_multiplier = points.streak - 3;
-            points.count += 5 * points_multiplier;
-          }
-          let new_bytes = bincode::serialize(&points).unwrap();
-          (*byte_data).copy_from_slice(&new_bytes[..]);
-          let added: bool = storage.put(&lump_id, &data).unwrap();
-          if added {
-            error!("error updating points");
-          }
-          if let Err(khm) = storage.journal_sync() {
-            error!("failed to sync {:?}", khm);
-          }
-          points.streak
+          if let Ok(mut points) = bincode::deserialize::<Points>(byte_data) {
+            points.count += 10;
+            points.streak += 1;
+            if points.streak > 3 {
+              let points_multiplier = points.streak - 3;
+              points.count += 5 * points_multiplier;
+            }
+            if let Ok(new_bytes) = bincode::serialize(&points) {
+              (*byte_data).copy_from_slice(&new_bytes[..]);
+              match storage.put(&lump_id, &data) {
+                Ok(added) => {
+                  if added {
+                    error!("error updating points");
+                  }
+                  if let Err(khm) = storage.journal_sync() {
+                    error!("failed to sync {:?}", khm);
+                  }
+                  points.streak
+                }, Err(ecn) => {
+                  error!("Something wrong with cannyls: {:?}", ecn);
+                  0
+                }
+              }
+            } else { 0 }
+          } else { 0 }
         } else {
           let points = Points { count: 10, streak: 1 };
-          let encoded: Vec<u8> = bincode::serialize(&points).unwrap();
-          let lump_data: LumpData = LumpData::new(encoded).unwrap();
-          let added: bool = storage.put(&lump_id, &lump_data).unwrap();
-          if !added {
-            error!("error on points initialization");
-          }
-          if let Err(khm) = storage.journal_sync() {
-            error!("failed to sync {:?}", khm);
-          }
-          1
+          if let Ok(encoded) = bincode::serialize(&points) {
+            if let Ok(lump_data) = LumpData::new(encoded) {
+              if let Ok(added) = storage.put(&lump_id, &lump_data) {
+                if !added {
+                  error!("error on points initialization");
+                }
+                if let Err(khm) = storage.journal_sync() {
+                  error!("failed to sync {:?}", khm);
+                }
+                1
+              } else {
+                error!("Something is wrong with cannyls");
+                0
+              }
+            } else { 0 }
+          } else { 0 }
         }
       }, Err(why) => {
         error!("Failed to get key: {:?}", why);
@@ -219,16 +252,19 @@ pub async fn break_streak( guild_id: u64
       Ok(mbdata) => {
       if let Some(mut data) = mbdata {
           let byte_data: &mut [u8] = data.as_bytes_mut();
-          let mut points : Points = bincode::deserialize(byte_data).unwrap();
-          points.streak = 0;
-          let new_bytes = bincode::serialize(&points).unwrap();
-          (*byte_data).copy_from_slice(&new_bytes[..]);
-          let added: bool = storage.put(&lump_id, &data).unwrap();
-          if added {
-            error!("error updating points");
-          }
-          if let Err(khm) = storage.journal_sync() {
-            error!("failed to sync {:?}", khm);
+          if let Ok(mut points) = bincode::deserialize::<Points>(byte_data) {
+            points.streak = 0;
+            if let Ok(new_bytes) = bincode::serialize(&points) {
+              (*byte_data).copy_from_slice(&new_bytes[..]);
+              if let Ok(added) = storage.put(&lump_id, &data) {
+                if added {
+                  error!("error updating points");
+                }
+                if let Err(khm) = storage.journal_sync() {
+                  error!("failed to sync {:?}", khm);
+                }
+              }
+            }
           }
         }
       }, Err(why) => {

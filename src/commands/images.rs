97eab4fa0aa058f3d46c0036d3d3de2d557248ab
Qeenon <codemasha@gmail.com>
2020-08-01 19:37:43 +0400

    sad or happy

--- src/commands/images.rs
+++ src/commands/images.rs
@@ -247,6 +247,36 @@ async fn angry(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
+async fn sad(ctx: &Context, msg: &Message) -> CommandResult {
+  if let Err(why) = msg.delete(&ctx).await {
+    error!("Error deleting original command {:?}", why);
+  }
+  let gifs = fetch_gifs(ctx, "sad anime", 50, "off").await?;
+  let mut rng = StdRng::from_entropy();
+  let val = rng.gen_range(0, 49);
+  msg.channel_id.send_message(ctx, |m|
+    m.embed(|e| e.color(0x3252e3)
+                .author(|a| a.icon_url(&msg.author.face()).name( format!("{} is sad", &msg.author.name) ))
+                .image(&gifs[val].media[0].get("gif").unwrap().url))).await?;
+  Ok(())
+}
+
+#[command]
+async fn happy(ctx: &Context, msg: &Message) -> CommandResult {
+  if let Err(why) = msg.delete(&ctx).await {
+    error!("Error deleting original command {:?}", why);
+  }
+  let gifs = fetch_gifs(ctx, "happy anime", 50, "off").await?;
+  let mut rng = StdRng::from_entropy();
+  let val = rng.gen_range(0, 49);
+  msg.channel_id.send_message(ctx, |m|
+    m.embed(|e| e.color(0x3252e3)
+                .author(|a| a.icon_url(&msg.author.face()).name( format!("{} is happy", &msg.author.name) ))
+                .image(&gifs[val].media[0].get("gif").unwrap().url))).await?;
+  Ok(())
+}
+
+#[command]
 async fn shrug(ctx: &Context, msg: &Message) -> CommandResult {
   if let Err(why) = msg.delete(&ctx).await {
     error!("Error deleting original command {:?}", why);

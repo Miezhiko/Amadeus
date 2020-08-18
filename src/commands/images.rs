930df42009f3dd817ffe88db9bbbb1123c5a94a6
Gilfanov Dmitriy (home) <gilfanovdmitriy@gmail.com>
2020-08-18 21:33:10 +0500

    ~pout + todo

--- src/commands/images.rs
+++ src/commands/images.rs
@@ -349,6 +349,21 @@ async fn cry(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
+async fn pout(ctx: &Context, msg: &Message) -> CommandResult {
+  if let Err(why) = msg.delete(&ctx).await {
+    error!("Error deleting original command {:?}", why);
+  }
+  let gifs = fetch_gifs(ctx, "pout anime", 50, "off").await?;
+  let mut rng = StdRng::from_entropy();
+  let val = rng.gen_range(0, 49);
+  msg.channel_id.send_message(ctx, |m|
+    m.embed(|e| e.color(0x3252e3)
+                .author(|a| a.icon_url(&msg.author.face()).name( format!("{} is pouting", &msg.author.name) ))
+                .image(&gifs[val].media[0].get("gif").unwrap().url))).await?;
+  Ok(())
+}
+
+#[command]
 async fn cringe(ctx: &Context, msg: &Message) -> CommandResult {
   if let Err(why) = msg.delete(&ctx).await {
     error!("Error deleting original command {:?}", why);

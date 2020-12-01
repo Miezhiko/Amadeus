56a1892fc0b5e6630caf9ade67345f5259f08761
Qeenon <codemasha@gmail.com>
2020-12-01 13:52:42 +0400

    eating dirt is nice and healthy for covid

---
+++ eat-some-dirt.sh
@@ -0,0 +1,13 @@
+sed -i 's|"flo-grpc",||g' Cargo.toml
+
+echo "nom nom nom"
+
+sed -i '/flo-grpc/,+3d' Cargo.toml
+
+echo "nom nom nom"
+
+echo "remember to revert those lines Cargo.toml before anything"
+
+echo "nom nom nom"
+
+echo "-_-"

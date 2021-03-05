520e59bdd3b72754a44ed375f3d558809830f334
Qeenon <codemasha@gmail.com>
2021-03-05 13:04:02 +0400

    Store SLED db handle in OnceCell

--- src/common/db/sled_info.rs
+++ src/common/db/sled_info.rs
@@ -1,15 +1,28 @@
 use eyre::{ WrapErr, Result };
 
+use once_cell::sync::OnceCell;
+
 static SLED: &str = "trees/info.sled";
+static SLED_DB: OnceCell<sled::Db> = OnceCell::new();
+
+fn get_db_handle() -> Result<&'static sled::Db> {
+  if let Some(existing_handle) = SLED_DB.get() {
+    Ok(existing_handle)
+  } else {
+    let sled = sled::open(SLED)?;
+    SLED_DB.set(sled).map_err(|_| eyre!("Failed to store db handle"))?;
+    get_db_handle()
+  }
+}
 
 pub fn store(key: &str, value: &str) -> Result<()> {
-  let sled = sled::open(SLED)?;
+  let sled = get_db_handle()?;
   sled.insert(key, value)?;
   Ok(())
 }
 
 pub fn read(key: &str) -> Result<String> {
-  let sled = sled::open(SLED)?;
+  let sled = get_db_handle()?;
   match sled.get(key) {
     Ok(Some(value)) => String::from_utf8(value.to_vec()).wrap_err("Failed to parse utf8"),
     Ok(None) => Err(eyre!("value not found")),
@@ -18,7 +31,7 @@ pub fn read(key: &str) -> Result<String> {
 }
 
 pub fn list() -> Result<String> {
-  let sled = sled::open(SLED)?;
+  let sled = get_db_handle()?;
   let mut result = vec![];
   for key in sled.iter().keys() {
     if let Ok(k) = key {
@@ -32,7 +45,7 @@ pub fn list() -> Result<String> {
 }
 
 pub fn delete(key: &str) -> Result<()> {
-  let sled = sled::open(SLED)?;
+  let sled = get_db_handle()?;
   sled.remove(key)?;
   Ok(())
 }

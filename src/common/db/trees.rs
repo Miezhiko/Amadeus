3eda1fc59b5c55d0638775bc64393b0aa8435c12
Miezhiko <Miezhiko@gmail.com>
2021-09-23 13:06:55 +0400

    Fix emoji roles! TODO: do optimization for same-size slices
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/common/db/trees.rs
+++ src/common/db/trees.rs
@@ -329,21 +329,25 @@ pub async fn update_roles( guild_id: &u64
   let u64_2: u128 = (*guild_id as u128) << 64 | *user_id as u128; // >
   let lump_id = LumpId::new(u64_2);
   if let Ok(mbdata) = storage.get(&lump_id) {
-    if mbdata.is_none() {
-      if let Ok(encoded) = bincode::serialize(roles) {
-        if let Ok(lump_data) = LumpData::new(encoded) {
-          match storage.put(&lump_id, &lump_data) {
-            Ok(added) => {
-              if !added {
-                error!("error on msg registration");
-              }
-            }, Err(not_added) => {
-              error!("error on msg registration {:?}", not_added);
+    if mbdata.is_some() {
+      let _ = storage.delete(&lump_id);
+      if let Err(khm) = storage.journal_sync() {
+        error!("failed to sync {:?}", khm);
+      }
+    }
+    if let Ok(encoded) = bincode::serialize(roles) {
+      if let Ok(lump_data) = LumpData::new(encoded) {
+        match storage.put(&lump_id, &lump_data) {
+          Ok(added) => {
+            if !added {
+              error!("error on msg registration");
             }
+          }, Err(not_added) => {
+            error!("error on msg registration {:?}", not_added);
           }
-          if let Err(khm) = storage.journal_sync() {
-            error!("failed to sync {:?}", khm);
-          }
+        }
+        if let Err(khm) = storage.journal_sync() {
+          error!("failed to sync {:?}", khm);
         }
       }
     }
@@ -385,6 +389,10 @@ pub async fn register_message( guild_id: &u64
         emoji_roles.insert(*emoji_id, *role_id);
         if let Ok(new_bytes) = bincode::serialize(&emoji_roles) {
           (*byte_data).copy_from_slice(&new_bytes[..]);
+          let _ = storage.delete(&lump_id);
+          if let Err(khm) = storage.journal_sync() {
+            error!("failed to sync {:?}", khm);
+          }
           match storage.put(&lump_id, &data) {
             Ok(added) => {
               if added {

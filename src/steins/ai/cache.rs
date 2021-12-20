30361327daac0e291d293f923b96bb0a76220a6d
Miezhiko <Miezhiko@gmail.com>
2021-12-20 12:31:51 +0400

    reduce activity
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/steins/ai/cache.rs
+++ src/steins/ai/cache.rs
@@ -46,7 +46,7 @@ static NLPRULE_RULES: &str = "nlprule/en_rules.bin";
 static CHANNEL_CACHE_MAX: u64 = 199;
 
 // TODO: store in some variable
-pub static ACTIVITY_LEVEL: AtomicU32 = AtomicU32::new(99);
+pub static ACTIVITY_LEVEL: AtomicU32 = AtomicU32::new(199);
 
 pub static CACHE_ENG: Lazy<Mutex<Chain<String>>> =
   Lazy::new(|| Mutex::new(Chain::new()));

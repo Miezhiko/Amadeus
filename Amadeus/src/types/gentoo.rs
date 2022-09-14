0ec74533aed22a1bff9ed7a425e914c3d310b8a2
Miezhiko <Miezhiko@gmail.com>
2022-09-14 12:01:44 +0400

    pull more information from bugzilla
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

---
+++ Amadeus/src/types/gentoo.rs
@@ -0,0 +1,19 @@
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+pub struct Bug {
+  pub assigned_to: String,
+  pub creation_time: String,
+  pub creator: String,
+  pub is_open: bool,
+  pub priority: String,
+  pub product: String,
+  pub resolution: String,
+  pub status: String,
+  pub summary: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+pub struct Bugs {
+  pub bugs: Vec<Bug>
+}

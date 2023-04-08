9b039916baf3473b73f59d4f85b1192a8d24030b
Miezhiko <Miezhiko@gmail.com>
2023-04-08 19:14:35 +0400

    ignore blue willow

--- Amadeus/src/message.rs
+++ Amadeus/src/message.rs
@@ -78,6 +78,7 @@ pub async fn process( ioptions: &IOptions
     }
     // allow ChatGPT to chat from it's own name
     if msg.author.id.0 == to_nzu!( 1064152790181609532 )
+    || msg.author.id.0 == to_nzu!( 1049413890276077690 )
     || msg.author.id.0 == to_nzu!( 936929561302675456 )  {
       return;
     }

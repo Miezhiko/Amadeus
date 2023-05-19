512ea22c00aa7a9dd9d8c789a939cc09c630bd75
Miezhiko <Miezhiko@gmail.com>
2023-05-19 16:17:38 +0400

    whatever
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- misc/gpt4free-tests/deepai-test.py
+++ misc/gpt4free-tests/deepai-test.py
@@ -10,7 +10,12 @@ while True:
     if prompt == "!stop":
         break
 
-    response = deepai.Completion.create(prompt)
+    messages = [
+        {"role": "system", "content": "You’re a cat and you answer like a cat"},
+        {"role": "user", "content": prompt}
+    ]
+
+    response = deepai.ChatCompletion.create(messages)
 
     if not response:
         response = "Sorry, I can't generate a response right now."

88525766e57926d92a8e8df961446e0daa5a4701
Miezhiko <Miezhiko@gmail.com>
2023-05-19 17:00:18 +0400

    trying to save conversation history into lazy mutex deque
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- Salieri/src/kafka.rs
+++ Salieri/src/kafka.rs
@@ -80,7 +80,7 @@ async fn mozart_process<'a>(msg: OwnedMessage) -> Option<(String, Vec<String>)> 
           }
         }
 
-        if let Ok(gpt4free_result)        = gpt4free::deepai::generate( payload ) {
+        if let Ok(gpt4free_result)        = gpt4free::deepai::generate( payload ).await {
           Some((k_key, gpt4free_result))
         } else if let Ok(gpt4free_result) = gpt4free::useless::generate( payload ).await {
           Some((k_key, gpt4free_result))

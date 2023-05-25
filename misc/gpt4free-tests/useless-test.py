46ba21c6efb752ba13abda3280e725db017b9699
Miezhiko <Miezhiko@gmail.com>
2023-05-25 11:17:34 +0400

    Vivaldi: add useless again
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- misc/gpt4free-tests/useless-test.py
+++ misc/gpt4free-tests/useless-test.py
@@ -5,14 +5,18 @@ import os
 
 from gpt4free import usesless
 
+mytoken = usesless.Account.create(logging=True)
+
 message_id = ""
 while True:
     prompt = input("Question: ")
     if prompt == "!stop":
         break
 
-    mytoken = usesless.Account.create(logging=True)
-    req = rspns = usesless.Completion.create(prompt=prompt, parentMessageId=message_id, token=mytoken)
+    systemContext = "You are a helpful assistant"
 
-    print(f"Answer: {{req[\"text\"]}}")
+    req = usesless.Completion.create(prompt=prompt, systemMessage=systemContext, parentMessageId=message_id, token=mytoken)
+
+    ans = req["text"]
+    print(f"Answer: {ans}")
     message_id = req["id"]

7ca50fc8dd15101cd90111265b4c80e8ec0edf32
Miezhiko <Miezhiko@gmail.com>
2023-08-14 13:37:45 +0400

    small chatrs api update
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- Strauss/src/chat.rs
+++ Strauss/src/chat.rs
@@ -17,7 +17,7 @@ async fn chat(msg_content: &str, user_id: u64) -> anyhow::Result<String> {
   info!("Generating chat response");
   let payload = process_message_for_gpt(msg_content);
   if user_id == 510368731378089984 {
-    chat::generate(&payload).await
+    chat::generate(&payload, "Amadeus", false).await
   } else {
     chat::chat(&payload, "Amadeus").await
   }

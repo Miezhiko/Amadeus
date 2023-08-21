ef3c8a526145bf5c780d2c8a71c7465895e51875
Miezhiko <Miezhiko@gmail.com>
2023-08-21 18:05:35 +0400

    MULTIGEN
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- Strauss/src/chat.rs
+++ Strauss/src/chat.rs
@@ -2,7 +2,6 @@ use crate::{
   types::ChatResponse,
   prelude::*,
   bert::{ LUKASHENKO
-        , process_message_for_gpt
         , chat::chat_gpt2_send }
 };
 
@@ -13,41 +12,70 @@ use std::{
   io::prelude::*
 };
 
-async fn chat(msg_content: &str, user_id: u64) -> anyhow::Result<String> {
-  info!("Generating chat response");
-  let payload = process_message_for_gpt(msg_content);
-  if user_id == 510368731378089984 {
-    match chat::generate(&payload, "Amadeus", false).await {
-      Ok(resp) => Ok(resp),
-      Err(why) => Ok(format!("chat: Failed to generate response: {why}"))
-    }
-  } else {
-    chat::chat(&payload, "Amadeus").await
-  }
-}
-
 async fn chat_send( msg: Option<u64>
                   , chan: u64
                   , something: String
                   , user_id: u64
                   , lsm: bool
                   , russian: bool ) -> anyhow::Result<()> {
-  match chat(something.as_str(), user_id).await {
-    Ok(result) => {
+  if user_id == 510368731378089984 {
+    if something.contains("MULTIGEN") {
+      let payload = something.replace("MULTIGEN ", "")
+                             .replace("MULTIGEN", "");
+      let resp_list = chat::generate_all(&payload, "Amadeus", false).await;
+      let temp_dir = std::env::temp_dir();
+      let mut lukashenko = UnixStream::connect(temp_dir.join(LUKASHENKO))?;
+      for (model_name, resp) in resp_list {
+        let resp_format = match resp {
+          Ok(r) => r,
+          Err(err) => format!("Failed: {err}")
+        };
+        let model_response = format!("**{model_name}**:\n{resp_format}");
+        let package = ChatResponse {
+          message: msg,
+          channel: chan,
+          response: model_response,
+          russian: false
+        };
+        let encoded = bincode::encode_to_vec(package, BINCODE_CONFIG)?;
+        lukashenko.write_all(&encoded)?;
+      }
+      Ok(())
+    } else {
+      let response = match chat::generate(&something, "Amadeus", false).await {
+        Ok(resp) => resp,
+        Err(why) => format!("Failed: {why}")
+      };
       let temp_dir = std::env::temp_dir();
       let mut lukashenko = UnixStream::connect(temp_dir.join(LUKASHENKO))?;
       let package = ChatResponse {
         message: msg,
         channel: chan,
-        response: result,
+        response: response,
         russian: false
       };
       let encoded = bincode::encode_to_vec(package, BINCODE_CONFIG)?;
       lukashenko.write_all(&encoded)?;
       Ok(())
-    }, Err(why) => {
-      error!("chat: Failed to generate response: {why}, using fallback to GPT2");
-      chat_gpt2_send(msg, chan, something, user_id, lsm, russian, 0).await
+    }
+  } else {
+    match chat::chat(something.as_str(), "Amadeus").await {
+      Ok(result) => {
+        let temp_dir = std::env::temp_dir();
+        let mut lukashenko = UnixStream::connect(temp_dir.join(LUKASHENKO))?;
+        let package = ChatResponse {
+          message: msg,
+          channel: chan,
+          response: result,
+          russian: false
+        };
+        let encoded = bincode::encode_to_vec(package, BINCODE_CONFIG)?;
+        lukashenko.write_all(&encoded)?;
+        Ok(())
+      }, Err(why) => {
+        error!("chat: Failed to generate response: {why}, using fallback to GPT2");
+        chat_gpt2_send(msg, chan, something, user_id, lsm, russian, 0).await
+      }
     }
   }
 }

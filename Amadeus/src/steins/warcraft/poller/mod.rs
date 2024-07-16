4a6d8030b7b44a9de04baf81505d72370ad092b3
Miezhiko <Miezhiko@gmail.com>
2024-07-16 15:40:09 +0400

    put GAMES to shared mutex
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- Amadeus/src/steins/warcraft/poller/mod.rs
+++ Amadeus/src/steins/warcraft/poller/mod.rs
@@ -5,8 +5,8 @@ pub mod checker;
 use crate::types::tracking::TrackingGame;
 
 use std::collections::HashMap;
-use tokio::sync::Mutex;
+use tokio::sync::RwLock;
 use once_cell::sync::Lazy;
 
-pub static GAMES: Lazy<Mutex<HashMap<String, TrackingGame>>>
-  = Lazy::new(|| Mutex::new(HashMap::new()));
+pub static GAMES: Lazy<RwLock<HashMap<String, TrackingGame>>>
+  = Lazy::new(|| RwLock::new(HashMap::new()));

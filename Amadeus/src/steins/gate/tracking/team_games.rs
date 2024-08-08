8c8d0240b7a3001011ac7a74f9f805f89f1ceb4a
Miezhiko <Miezhiko@gmail.com>
2024-08-08 17:20:16 +0400

    add team games ticks to log
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- Amadeus/src/steins/gate/tracking/team_games.rs
+++ Amadeus/src/steins/gate/tracking/team_games.rs
@@ -115,6 +115,7 @@ pub async fn activate_games_tracking(
         }
       }
 
+      info("team games: tick");
       let our_gsx = checker::check( &ctx_clone
                                   , options_clone.guild
                                   , &rqcl

7e33770feba8a0b73a6b86e76241729c9e7f8da1
Miezhiko <Miezhiko@gmail.com>
2024-09-29 21:32:53 +0400

    reduce night poll time

--- Amadeus/src/steins/gate/tracking/team_games.rs
+++ Amadeus/src/steins/gate/tracking/team_games.rs
@@ -29,7 +29,7 @@ use std::{ time
 use rand::Rng;
 
 const DAY_TIMEOUT_SECS: u32   = 70;
-const NIGHT_TIMEOUT_SECS: u32 = 40;
+const NIGHT_TIMEOUT_SECS: u32 = 30;
 
 const DAY_TIMEOUT: time::Duration   = time::Duration::from_secs(DAY_TIMEOUT_SECS as u64);
 const NIGHT_TIMEOUT: time::Duration = time::Duration::from_secs(NIGHT_TIMEOUT_SECS as u64);
